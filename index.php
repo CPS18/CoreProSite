d63810a1cf4bf6f515f8fc06adeb2e55395150fc
Zoltan <46491198+CPS18@users.noreply.github.com>
2019-04-13 13:48:41 +0200

    Created navbar and favicon

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title -->
+    <title>Core Pro Software</title>
+    <!-- Favicon -->
+    <meta name="msapplication-TileColor" content="#2b5797">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/fav_icon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/fav_icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/fav_icon/favicon-16x16.png">
+    <link rel="manifest" href="assets/fav_icon/site.webmanifest">
+    <link rel="mask-icon" href="assets/fav_icon/safari-pinned-tab.svg" color="#000000">
+    <!-- FaviconEnd -->
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- /Preloader -->
+
+    <!-- Header Area Start -->
+    <header class="header-area">
+        <div class="classy-nav-container breakpoint-off">
+            <div class="container">
+                <!-- Classy Menu -->
+                <nav class="classy-navbar justify-content-between" id="conferNav">
+
+                    <!-- Logo 140x19 -->
+                    <a class="nav-brand" href="./index.php"><img src="assets/img/wide_icon.png" alt="" height="40" width="240"></a>
+
+                    <!-- Navbar Toggler -->
+                    <div class="classy-navbar-toggler">
+                        <span class="navbarToggler"><span></span><span></span><span></span></span>
+                    </div>
+
+                    <!-- Menu -->
+                    <div class="classy-menu">
+                        <!-- Menu Close Button -->
+                        <div class="classycloseIcon">
+                            <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                        </div>
+                        <!-- Nav Start -->
+                        <div class="classynav">
+                            <ul id="nav">
+                                <li class="active"><a class="effect" href="index.php">Home</a></li>
+                                <li><a class="effect" href="speakers.html">Services</a></li>
+                                <li><a class="effect" href="blog.html">Company</a></li>
+                                <li><a  class="effect"href="#"><img src="assets/img/usa.png" height="20" width="20">EN</a>
+                                    <ul class="dropdown">
+                                        <li><a style="border:none  !important;" class="effect dropback" href="index.html"><img src="assets/img/HungaryFlag.png" height="20" width="20"> HU</a></li>
+                                        <li><a class="effect dropback" href="about.html"><img src="assets/img/serbia.png" height="20" width="20"> SRB</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <!-- Contact Button -->
+                            <a href="#" class="btn confer-btn mt-3 mt-lg-0 ml-3 ml-lg-5">CONTACT US <i class="zmdi zmdi-long-arrow-down"></i></a>
+                        </div>
+                        <!-- Nav End -->
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <!-- Header Area End -->
+
+        <!-- jQuery 2.2.4 -->
+        <script src="js/jquery.min.js"></script>
+        <!-- Bootstrap -->
+        <script src="js/bootstrap.min.js"></script>
+
+    </body>
+    
+    </html>
